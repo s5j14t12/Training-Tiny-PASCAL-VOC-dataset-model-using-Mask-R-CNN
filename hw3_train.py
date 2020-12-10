897e3de1601c9638f74e2334b3fc8540954fa4a3
s5j14t12 <55782757+s5j14t12@users.noreply.github.com>
2020-12-11 07:45:59 +0800

    Update hw3_train.py

--- hw3_train.py
+++ hw3_train.py
@@ -407,7 +407,7 @@ if __name__ == '__main__':
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=40,
+                epochs=8,
                 layers='heads',
                 augmentation=augmentation)
 
@@ -416,7 +416,7 @@ if __name__ == '__main__':
     print("Fine tune Resnet stage 4 and up")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=120,
+                epochs=24,
                 layers='4+',
                 augmentation=augmentation)
 
@@ -425,7 +425,7 @@ if __name__ == '__main__':
     print("Fine tune all layers")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE / 10,
-                epochs=160,
+                epochs=32,
                 layers='all',
                 augmentation=augmentation)
     print("train complete!")
